66638342596b3adc12cbd463502717a4f1b8f814
Dobby89 <robin.oneill@ao.com>
2017-06-23 17:12:51 +0100

    Remove example test

--- test/test.spec.js
+++ test/test.spec.js
@@ -5,14 +5,6 @@ var expect = require('chai').expect;
 
 import AoSpinner from '../src/scripts/index';
 
-describe('Array', function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
-            assert.equal(-1, [1,2,3].indexOf(4));
-        });
-    });
-});
-
 describe('aoSpinner', function () {
     describe('given a node', function () {
         var targetElement = document.createElement('div');
